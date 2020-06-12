8cd17a667d878d35eed9794b04dcbc9c362bc3cc
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2020-06-12 15:39:16 +0200

    this should not be passing

---
+++ src/__tests__/mocking002.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+const log = console.log;
+
+const assert = require("assert");
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
+
+test("returns winner", () => {
+  const originalGetWinner = utils.getWinner;
+
+  utils.getWinner = (player1, player2) => player1;
+
+  const winner = thumbWar("Stephen A. Ingram", "Kerstin Dengl");
+
+  expect(winner).toBe("Stephen A. Ingram");
+
+  // cleanup
+
+  utils.getWinner = originalGetWinner;
+});
