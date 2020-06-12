78a0c099c82032f29b654fa37683803de48bcf23
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2020-06-12 14:26:22 +0200

    get a winner and add prettier etc

--- src/mocking001.js
+++ src/mocking001.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-console */
+const log = console.log;
+
 const assert = require("assert");
 const thumbWar = require("./thumb-war");
 const utils = require("./utils");
+
+const winner = thumbWar("Stephen A. Ingram", "Kerstin Dengl");
+
+log(winner);
