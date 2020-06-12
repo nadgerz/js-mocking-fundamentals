44472558a7029ca0e899ac738de37afad43c6f35
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2020-06-12 16:27:26 +0200

    and now we mocked it all

--- src/mocking002.js
+++ src/mocking002.js
@@ -7,10 +7,12 @@ const utils = require("./utils");
 
 function fn(impl) {
   const mockFn = (...args) => {
-    console.log(args);
+    // log(args);
+    mockFn.mock.calls.push(args);
     return impl(...args);
   };
 
+  mockFn.mock = { calls: [] };
   return mockFn;
 }
 
@@ -21,6 +23,20 @@ utils.getWinner = fn((player1, player2) => player1);
 const winner = thumbWar("Stephen A. Ingram", "Kerstin Dengl");
 
 assert.strictEqual(winner, "Stephen A. Ingram");
+// log(utils.getWinner.mock.calls);
+assert.deepStrictEqual(utils.getWinner.mock.calls, [
+  ["Stephen A. Ingram", "Kerstin Dengl"],
+  ["Stephen A. Ingram", "Kerstin Dengl"],
+]);
+
+/*
+AssertionError [ERR_ASSERTION]: Values have same structure but are not reference-equal:
+
+assert.strictEqual(utils.getWinner.mock.calls, [
+  ["Stephen A. Ingram", "Kerstin Dengl"],
+  ["Stephen A. Ingram", "Kerstin Dengl"],
+]);
+ */
 
 // cleanup
 utils.getWinner = originalGetWinner;
